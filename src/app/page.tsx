0c53d0daece18c8526dae54f5186527c5ed22aae
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-10-22 12:06:10 +0530

    Fresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import MoleculeGenerator from "./model";
 
 export default function Home() {
   return (
     <>
       <div className="h-screen">
         <p>Protein Bind</p>
-        <MoleculeGenerator />
       </div>
     </>
   );
